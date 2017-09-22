718766e59b0597f1c01495bad9e2331707b56e3b
Joerg Roedel <jroedel@suse.de>
2017-09-22 14:38:35 +0200

    git-recent: Add option parsing and remote branches
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- git-recent.cc
+++ git-recent.cc
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+#include <getopt.h>
 #include <git2.h>
 
 struct branch {
@@ -22,23 +23,82 @@ struct branch {
 	}
 };
 
+enum {
+	OPTION_HELP,
+	OPTION_ALL,
+	OPTION_REMOTE,
+};
+
+static struct option options[] = {
+	{ "help",		no_argument,		0, OPTION_HELP           },
+	{ "all",		no_argument,		0, OPTION_ALL            },
+	{ "remote",		required_argument,	0, OPTION_REMOTE         },
+	{ 0,			0,			0, 0                     }
+};
+
+static void usage(const char *cmd)
+{
+	std::cout << "Usage: " << cmd << " [options]" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "  --help, -h             Print this help message" << std::endl;
+	std::cout << "  --all, -a              Also show remote branches" << std::endl;
+	std::cout << "  --remote, -r <remote>  Only show branches of a given remote" << std::endl;
+}
+
+bool is_prefix(std::string str, std::string prefix)
+{
+	if (str.size() < prefix.size())
+		return false;
+
+	return (str.substr(0, prefix.size()) == prefix);
+}
+
 int main(int argc, char **argv)
 {
+	git_branch_t flags = GIT_BRANCH_LOCAL;
 	std::string::size_type max_len = 0;
 	std::vector<branch> results;
 	git_repository *repo = NULL;
 	git_branch_iterator *it;
 	git_branch_t ref_type;
 	git_reference *ref;
+	std::string prefix;
 	int error;
 
+	while (true) {
+		int c, opt_idx;
+
+		c = getopt_long(argc, argv, "har:", options, &opt_idx);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case OPTION_HELP:
+		case 'h':
+			usage(argv[0]);
+			return 0;
+			break;
+		case OPTION_ALL:
+		case 'a':
+			flags = GIT_BRANCH_ALL;
+			break;
+		case OPTION_REMOTE:
+		case 'r':
+			flags = GIT_BRANCH_REMOTE;
+			prefix = std::string(optarg) + '/';
+			break;
+		default:
+			usage(argv[0]);
+			return 1;
+		}
+	}
 	git_libgit2_init();
 
 	error = git_repository_open(&repo, ".");
 	if (error < 0)
 		goto err;
 
-	error = git_branch_iterator_new(&it, repo, GIT_BRANCH_LOCAL);
+	error = git_branch_iterator_new(&it, repo, flags);
 	if (error < 0)
 		goto err;
 
@@ -54,6 +114,9 @@ int main(int argc, char **argv)
 
 		sname = name;
 		max_len = std::max(max_len, sname.size());
+
+		if (!is_prefix(sname, prefix))
+			continue;
 
 		oid = git_reference_target(ref);
 		if (oid == NULL) {
